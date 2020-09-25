7adc4bb56129aad34d213da7b9acf4f2a7e327cc
Jarryd Palzenberger <jarrydpalz@gmail.com>
2020-09-25 12:58:54 +0100

    Step 2 - Add Collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {Task} from './Task';
+import { Task } from './Task';
+import { TasksCollection } from '../api/TasksCollection';
+import { useTracker } from 'meteor/react-meteor-data';
 
-const tasks = [
-  {_id:1, text:"Clean my room"},
-  {_id:2, text:"Book a holiday"},
-  {_id:3, text: "Apply for jobs"},
-];
+export const App = () => {
 
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
+  return (
+    <div >
+      <h1>Welcome to Meteor!</h1>
 
-    <ul>
-      {tasks.map(task => <Task key={task._id} task={task}/>)}
-    </ul>
-    
-  </div>
-);
+      <ul>
+        {tasks.map(task => <Task key={task._id} task={task} />)}
+      </ul>
+
+    </div >
+
+  );
+
+};
