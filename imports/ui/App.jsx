8427354882f4a6e4107310b478429dcf5da358a5
Jarryd Palzenberger <jarrydpalz@gmail.com>
2020-09-25 13:33:52 +0100

    Step 3 - Form to add tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Task } from './Task';
 import { TasksCollection } from '../api/TasksCollection';
 import { useTracker } from 'meteor/react-meteor-data';
+import {TaskForm} from '../ui/TaskForm';
 
 export const App = () => {
 
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, {sort: {createdAt: -1} }).fetch());
 
   return (
     <div >
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm/>
 
       <ul>
         {tasks.map(task => <Task key={task._id} task={task} />)}
